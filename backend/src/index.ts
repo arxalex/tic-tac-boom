07cc746d6c64c6702092f72581a7edcff06b7e78
Oleksandr Yushchenko <arxalex@arxalex.com>
2025-06-23 19:34:12 +0300

    fix undefined -> null

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -139,7 +139,7 @@ app.post('/link/update', async (c) => {
     return c.json({error: 'Query not specified'}, 404)
   }
 
-  const query = `update ${tables.members} set name = ?, score = ? where id = ? and pass = ? and memberid = ?`;
+  const query = `update ${tables.links} set name = ?, score = ? where id = ? and pass = ? and memberid = ?`;
   const result = await c.env.DB.prepare(query).bind(data.name ?? null, data.score ?? null, data.id, data.pass, data.memberid).run();
   return c.json({
     response: result.success
